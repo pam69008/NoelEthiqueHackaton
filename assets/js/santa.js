43d3331cef66407f470a4b09af7b3de225c562f5
julien <julienl6900669006@gmail.com>
2019-12-19 20:27:38 +0100

    santa sound

--- assets/js/santa.js
+++ assets/js/santa.js
@@ -4,8 +4,6 @@ function getRandomInt(max) {
 
 $(document).ready(function () {
 
-
-
 // //Position tête poison gauche ou droite
 // // document.getElementById("fish")
 //     function myMove() {
@@ -16,7 +14,7 @@ $(document).ready(function () {
         var posMax = [container.offsetWidth - elem.offsetWidth, container.offsetHeight - elem.offsetHeight];
         var position = [getRandomInt(posMax[0]), getRandomInt(posMax[1])];
 
-        var id = setInterval(frame, 1);
+        var id = setInterval(frame, 100);
 
         function frame() {
 
@@ -36,11 +34,11 @@ $(document).ready(function () {
 
             var directionHB = 1;
             if (pos[1] > position[1]) {
-                directionHB *= -5; // reverses current direction
+                directionHB *= -1; // reverses current direction
                 // elem.style.transform = "rotate3d(0.18,1,0,170deg)";
             }
             if (pos[1] < position[1]) {
-                directionHB *= 5;
+                directionHB *= 1;
                 // vers le bas
                 // elem.style.transform = "rotate3d(0,0,1,25deg)";
             }
@@ -52,12 +50,12 @@ $(document).ready(function () {
             elem.style.top = pos[1] + "px";
             elem.style.left = pos[0] + "px";
 
-
             if (pos[0] === position[0]) {
                 position = [getRandomInt(posMax[0]), getRandomInt(posMax[1])];
             }
-
-
         }
 
+    $( "#santa" ).click(function (){
+        $("#playerOhOh")[0].play();
+    });
 })
