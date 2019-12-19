5a7025d545d6612128a2067b07ad9772fc92f4d7
julien <julienl6900669006@gmail.com>
2019-12-19 20:05:49 +0100

    ajout du pere noel fou

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,8 @@ Encore
     .addEntry('app', './assets/js/app.js')
     .addEntry('test', './assets/js/script.min.js')
     .addEntry('navbarMec' , './assets/js/navbar.js')
+    .addEntry('santaDude' , './assets/js/santa.js')
+
     //.addEntry('page2', './assets/js/page2.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
