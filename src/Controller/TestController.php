5541b2680073bee5a803f085362d6c647b971b3e
Pierre-Alain Monney <pierrealain.monney@gmail.com>
2019-12-20 09:07:12 +0100

    mise a jour de la route et du titre

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -8,12 +8,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class TestController extends AbstractController
 {
     /**
-     * @Route("/test", name="test")
+     * @Route("/", name="test")
      */
     public function index()
     {
-        return $this->render('test/index.html.twig', [
-            'controller_name' => 'TestController',
-        ]);
+        return $this->render('test/index.html.twig');
     }
 }
